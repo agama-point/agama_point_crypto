2d548620e480a7bf19416d7de8ed3674cc0b044c
Honza S. Copak <bonum.comunae@volny.cz>
2023-07-01 10:06:08 +0200

    add rgb data to image_tools

--- crypto_agama/agama_image_tools.py
+++ crypto_agama/agama_image_tools.py
@@ -5,12 +5,12 @@ from PIL import Image
 import numpy as np
 
 
-__version__ = "0.2.0"
-# png - gray scale 
+__version__ = "0.2.5"
+# png - gray scale / rgb-alpha color 
 # bmp - "1bit"
 
 
-def hexa_noise_to_png(hex_data, image_path):
+def hexa_noise_to_png(hex_data,image_path):
     byte_data = bytes.fromhex(hex_data)    
     num_data = np.frombuffer(byte_data, dtype=np.uint8)
     """
@@ -41,7 +41,7 @@ def noise_png_to_hexa(image_path):
     return hex_data
 
 
-def hexa_noise_to_bmpline(hex_data, image_path):
+def hexa_noise_to_bmpline(hex_data,image_path):
     byte_data = bytes.fromhex(hex_data) # hex2bytesArr
     num_data = np.frombuffer(byte_data, dtype=np.uint8) # 2nums
 
@@ -66,7 +66,7 @@ def noise_bmpline_to_hexa(image_path):
     return hex_data
 
 
-def hexa_arr_noise_to_bmp(hex_array_data, image_path):
+def hexa_arr_noise_to_bmp(hex_array_data,image_path):
     num_elements = len(hex_array_data)
     num_pixels = max(len(hex_data) * 4 for hex_data in hex_array_data)
 
@@ -97,3 +97,72 @@ def noise_bmp_to_hexa_arr(image_path):
         hex_array_data.append(hex_data)
 
     return hex_array_data
+
+
+# ----------------- pgt/.png ---------------
+def print_rgb(img,width=32,height=32,xy=True,all=True,len=32):
+    print("DEBUG print_rgb",int(len/width))
+    if all:
+        jmax = height
+    else:
+        jmax = int(len/(width-1)) # ¯_(ツ)_/¯
+    
+    for j in range(jmax):
+        for i in range(width): #min(width,len)):
+            #print(f"[{i}{j}]", end="")
+            try:
+                r, g, b, a = img.get_at((i, j))
+                if xy:
+                    print(f"[{i}{j}]",r, end=" ")
+                else:
+                    print(f"[{r},{g},{b}]", end=" ") # alpha ¯_(ツ)_/¯
+            except:
+                print(f"[{i}{j}]","?", end="!")
+        print()
+    print()
+
+
+def infilt_patt(img,bit_patt,width=32,height=32,ch ="R"):
+    print("DEBUG infilt_patt",int(len(bit_patt)/(width-1)))
+    for j in range(int(len(bit_patt)/(width-1))+1):
+        for i in range(width):
+            try:
+                r, g, b, a = img.get_at((i, j))
+                if bit_patt[i+j*width] == "1":
+                    if ch=="R":
+                        img.set_at((i, j),(r+1,g,b,a))
+                    if ch=="G":
+                        img.set_at((i, j),(r,g+1,b,a))
+                    if ch=="B":
+                        img.set_at((i, j),(r,g,b+1,a))
+                
+                print(f"[{i},{j},{i+j*width}]",bit_patt[i+j*width],r, end="")
+            except:
+                print(f"[{i},{j}]", end="!")
+        print()
+      
+    return img
+
+
+def parse_patt(img,len_bit_patt=255,width=32,height=32,ch="R"):
+    # pattern = []
+    pattern = ""
+    for j in range(int(len_bit_patt/(width-1)+1)):
+        for i in range(width):
+            try:
+                r, g, b, a = img.get_at((i, j))
+                if ch=="R":
+                    if r%2==0: 
+                        #pattern.append(0)
+                        pattern +="0"
+                    else:
+                        #pattern.append(1)
+                        pattern +="1"
+                if ch=="G":
+                    pattern += "0" if g % 2 == 0 else "1"
+                if ch=="B":
+                    pattern += "0" if b % 2 == 0 else "1"
+            except:
+                print(f"[{i},{j}]", end="!")
+    print()
+    return pattern
