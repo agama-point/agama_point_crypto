50004ab397ff05037a981fef17a74ab839e02bae
Honza S. Copak <bonum.comunae@volny.cz>
2024-01-29 11:19:05 +0100

    add seed examples from cryptos

--- priv_data.py
+++ priv_data.py
@@ -1,8 +1,17 @@
 words_book = "army van defense carry jealous true garbage claim echo media make crunch"
 words_tbtc = "major easy ignore body rule stay gorilla eager arch actor scan thank" # www
+words_cryptos12 = "practice display use aisle armor salon glue okay sphere rather belt mansion"
+words_cryptos15 = "jealous silver churn hedgehog border physical market parent hungry design cage lab drill clay attack"
+words_trezor = "inside label dolphin print depart zone earth fix senior identify net surprise"
 words_test = "..."
 
 seeds = (words_book, words_tbtc)
 
 def get_words(): return words_tbtc
 def get_seeds(): return seeds # words_tbtc
+
+"""
+from priv_data import words_book
+words = words_book
+...
+"""
