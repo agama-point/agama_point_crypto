9e0997974a0ce2ab12c07e74c3e71f0ee653ced9
Honza S. Copak <bonum.comunae@volny.cz>
2024-06-01 21:47:39 +0200

    added test entropy

--- priv_data.py
+++ priv_data.py
@@ -3,12 +3,20 @@ w_000000 = "abandon abandon abandon abandon abandon abandon abandon abandon aban
 w_111111 = "zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo wrong" # "11111" last chsm: wrong winter ...
 w_808080 = "letter advice cage absurd amount doctor acoustic avoid letter advice cage above"
 
+# https://iancoleman.io/bip39/
 words_book = "army van defense carry jealous true garbage claim echo media make crunch"
+entro_book = "0c1e24e5917779d297e14d45f14e1a1a"
+
+words_trezor = "inside label dolphin print depart zone earth fix senior identify net surprise"
+entro_trezor = "752f85035563adff915ac0c3ae1252ed"
+
 words_tbtc = "major easy ignore body rule stay gorilla eager arch actor scan thank" # www
+
 words_cryptos12 = "practice display use aisle armor salon glue okay sphere rather belt mansion"
 words_cryptos15 = "jealous silver churn hedgehog border physical market parent hungry design cage lab drill clay attack"
-words_trezor = "inside label dolphin print depart zone earth fix senior identify net surprise"
+
 words_test = "..."
+
 
 seeds = (words_book, words_tbtc)
 
