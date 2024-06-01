cff7f20d23b1e7e75091b46ce9ac7e88aa5cf1d6
Honza S. Copak <bonum.comunae@volny.cz>
2024-06-01 21:17:32 +0200

    add test 808080

--- priv_data.py
+++ priv_data.py
@@ -1,3 +1,8 @@
+# bip39 vectors: https://github.com/trezor/python-mnemonic/blob/master/vectors.json
+w_000000 = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about"
+w_111111 = "zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo wrong" # "11111" last chsm: wrong winter ...
+w_808080 = "letter advice cage absurd amount doctor acoustic avoid letter advice cage above"
+
 words_book = "army van defense carry jealous true garbage claim echo media make crunch"
 words_tbtc = "major easy ignore body rule stay gorilla eager arch actor scan thank" # www
 words_cryptos12 = "practice display use aisle armor salon glue okay sphere rather belt mansion"
